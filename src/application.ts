3ad044afcfcf9057f031610bcbac7d2842bae0bd
Anes <anesmsaddak@gmail.com>
2023-07-20 12:51:13 +0100

    first commit

---
+++ src/application.ts
@@ -0,0 +1,63 @@
+import {BootMixin} from '@loopback/boot';
+import {ApplicationConfig} from '@loopback/core';
+
+import {
+  RestExplorerBindings,
+  RestExplorerComponent,
+} from '@loopback/rest-explorer';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
+import {ServiceMixin} from '@loopback/service-proxy';
+import path from 'path';
+import {MySequence} from './sequence';
+
+import {AuthenticationComponent} from '@loopback/authentication';
+import {
+  JWTAuthenticationComponent,
+  SECURITY_SCHEME_SPEC,
+  UserServiceBindings,
+} from '@loopback/authentication-jwt';
+import {DbDataSource} from './datasources';
+
+
+export {ApplicationConfig};
+
+export class TodoListApplication extends BootMixin(
+  ServiceMixin(RepositoryMixin(RestApplication)),
+) {
+  constructor(options: ApplicationConfig = {}) {
+    super(options);
+
+    // Set up the custom sequence
+    this.sequence(MySequence);
+
+    // Set up default home page
+    this.static('/', path.join(__dirname, '../public'));
+
+    // Customize @loopback/rest-explorer configuration here
+    this.configure(RestExplorerBindings.COMPONENT).to({
+      path: '/explorer',
+    });
+    this.component(RestExplorerComponent);
+
+    this.projectRoot = __dirname;
+    // Customize @loopback/boot Booter Conventions here
+    this.bootOptions = {
+      controllers: {
+        // Customize ControllerBooter Conventions here
+        dirs: ['controllers'],
+        extensions: ['.controller.js'],
+        nested: true,
+      },
+    };
+
+    //Authentication part:
+    //Mount authentication system
+    this.component(AuthenticationComponent);
+    //jwt component
+    this.component(JWTAuthenticationComponent);
+    //Datasource binding
+    this.dataSource(DbDataSource, UserServiceBindings.DATASOURCE_NAME)
+    
+  }
+}
---
+++ src/sequence.ts
@@ -0,0 +1,10 @@
+import {MiddlewareSequence} from '@loopback/rest';
+// import {
+//   AuthenticateFn,
+//   AuthenticationBindings,
+//   AUTHENTICATION_STRATEGY_NOT_FOUND,
+//   USER_PROFILE_NOT_FOUND,
+// } from '@loopback/authentication';
+// import {inject} from '@loopback/core';
+
+export class MySequence extends MiddlewareSequence {}
